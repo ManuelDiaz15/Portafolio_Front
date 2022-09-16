c8b5fb6110d011293ad36c04316b670f63f7d425
Manuel <Mana.diaz@duocuc.cl>
2022-09-16 02:28:36 -0400

    Avance Page Edit v0.3 Agregando documentación al Código

--- src/views/Inicio/EditView.vue
+++ src/views/Inicio/EditView.vue
@@ -1,7 +1,10 @@
 <template>
+    <!-- Cuerpo de la Page -->
     <body class="color_back1_edit">
+        <!-- Lo que esta haciendo el Container XL es centrar el div principal, abarcando width: 100%; margin-right: auto; margin-left: auto; -->
         <div class="container-xl containerxl_padding" >
-         <div class="row-0 justify-content-center color_back2_edit">
+
+         <div class="color_back2_edit">
             <div class="focused">
                 <div class="color_top_edit">
                     <h4> ID: 001 Transcripción Call Center: Ripley. Ejecutivo: Felipe Morales Fecha: 25/08/2022 </h4>
@@ -10,12 +13,12 @@
                 <div class="focused color_speaker_edit1">
                     <div>
                        <!--  <v-img contain alt="Alloxentric Agent" src="@/assets/Agent.png" max-height="50"></v-img> --> 
-                        <p class="p text-left"> <strong>Agent</strong></p>  
+                        <p class="p text-left"> <strong>Agent</strong></p> <!-- Se utiliza strong para dejar en negrita el texto -->
                         <p class="p text-left">
                             Thank you for calling ABC Company. My name is Ashley. How may I help you today?
                         </p>
                     </div>
-                    <div class="input-inferior row-0 justify-content-center">
+                    <div class="input-inferior">
                         <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
                     </div>
                 </div>
@@ -27,7 +30,7 @@
                         I am calling because I received a Wrong bill. I just paid my phone bill two days ago and my payment is not reflected in the bill.
                         </p>
                     </div>
-                    <div class="input-inferior row-0 justify-content-center">
+                    <div class="input-inferior">
                         <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
                     </div>
                 </div>
@@ -39,7 +42,7 @@
                             Thank you for calling ABC Company. My name is Ashley. How may I help you today?
                         </p>
                     </div>
-                    <div class="input-inferior row-0 justify-content-center">
+                    <div class="input-inferior">
                         <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
                     </div>
                 </div>
@@ -51,13 +54,12 @@
                         I am calling because I received a Wrong bill. I just paid my phone bill two days ago and my payment is not reflected in the bill.
                         </p>
                     </div>
-                    <div class="input-inferior row-0 justify-content-center">
+                    <div class="input-inferior">
                         <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
                     </div>
                 </div>
             </div> 
         </div>
-        
     </div>
     </body>
  </template>
