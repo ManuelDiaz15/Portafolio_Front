57cf9ae9f33da2846208e1136834ec1083c79ab4
Manuel <Mana.diaz@duocuc.cl>
2022-09-15 22:22:08 -0400

    Avance Page Edit v0.1

--- src/views/Inicio/EditView.vue
+++ src/views/Inicio/EditView.vue
@@ -1,32 +1,42 @@
 <template>
     <body>
         <div class="container-xl" >
-        <div class="row-0 justify-content-center" id="rectangulo">
+        <div class="row-0 justify-content-center">
             <div>
-                <h2> ID: 001 Transcripción Call Center Ripley.    Ejecutivo : Felipe Morales Fecha: 25/08/2022 </h2>
+                <div  class="cont_fondo">
+                    <h3> ID: 001 Transcripción Call Center: Ripley. Ejecutivo: Felipe Morales Fecha: 25/08/2022 </h3>
+                </div>
                 <div>
+                    <p class="p text-left"> Speaker1 :</p>  
                     <p class="p text-left">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, corrupti quae deserunt qui totam unde
-                    mollitia praesentium reprehenderit laudantium ducimus ab atque recusandae aliquam suscipit accusamus
-                    neque omnis <a class="text-link" href="">dignissimos labore</a>.
-                </p>
-                <p class="p text-left">
-                    Debitis, corrupti quae deserunt qui totam unde mollitia <a class="text-link" href="">dorime interimo
-                        adapare dorime</a> praesentium reprehenderit laudantium ducimus ab atque recusandae aliquam suscipit
-                    accusamus neque omnis.
-                </p>
+                    Debitis, corrupti quae deserunt qui totam unde mollitia accusamus neque omnis.
+                    </p>
+                
+            
+               
                 </div>
             </div>    
+            <div class="input-inferior row-0 justify-content-center">
+              <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
+            </div>
         </div>
-       
     </div>
     </body>
  </template>
+
 <style>
-    .rectangulo {
-     width: 250px; 
-     height: 100px; 
-     border: 3px solid #555;
-     background: #428bca;
+    .input-inferior input{
+    border:blue;
+    padding: 10px;
+    margin: auto;
+    width: 100%;
+    border-radius: 15px;
+    font-size: 16px;
+}
+.cont_fondo {
+    background: #b8b7b6;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    border-radius: 15px;
 }
 </style>
