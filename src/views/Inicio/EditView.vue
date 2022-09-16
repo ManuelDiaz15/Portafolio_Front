2710891e1162eef15c71f1a4de2ce355680c0c9a
Manuel <Mana.diaz@duocuc.cl>
2022-09-16 00:36:31 -0400

    Avance Page Edit v0.2

--- src/views/Inicio/EditView.vue
+++ src/views/Inicio/EditView.vue
@@ -1,42 +1,79 @@
 <template>
-    <body>
-        <div class="container-xl" >
-        <div class="row-0 justify-content-center">
-            <div>
-                <div  class="cont_fondo">
-                    <h3> ID: 001 Transcripción Call Center: Ripley. Ejecutivo: Felipe Morales Fecha: 25/08/2022 </h3>
+    <body class="color_back1_edit">
+        <div class="container-xl containerxl_padding" >
+         <div class="row-0 justify-content-center color_back2_edit">
+            <div class="focused">
+                <div class="color_top_edit">
+                    <h4> ID: 001 Transcripción Call Center: Ripley. Ejecutivo: Felipe Morales Fecha: 25/08/2022 </h4>
                 </div>
-                <div>
-                    <p class="p text-left"> Speaker1 :</p>  
-                    <p class="p text-left">
-                    Debitis, corrupti quae deserunt qui totam unde mollitia accusamus neque omnis.
-                    </p>
-                
-            
-               
+                <div class="linea"></div>
+                <div class="focused color_speaker_edit1">
+                    <div>
+                        <p class="p text-left"> <strong>Agent</strong></p>  
+                        <p class="p text-left">
+                            Thank you for calling ABC Company. My name is Ashley. How may I help you today?
+                        </p>
+                    </div>
+                    <div class="input-inferior row-0 justify-content-center">
+                        <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
+                    </div>
                 </div>
-            </div>    
-            <div class="input-inferior row-0 justify-content-center">
-              <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
-            </div>
+                <div class="linea"></div>
+                <div class="focused color_speaker_edit1">
+                    <div >
+                        <p class="p text-left"> <strong>Customer</strong></p>  
+                        <p class="p text-left">
+                        I am calling because I received a Wrong bill. I just paid my phone bill two days ago and my payment is not reflected in the bill.
+                        </p>
+                    </div>
+                    <div class="input-inferior row-0 justify-content-center">
+                        <input type="text" class="input"  v-on:keyup.enter="Agregar " placeholder="Enviar Transcripción" v-model="eviarContenido">
+                    </div>
+                </div>
+            </div> 
         </div>
+        
     </div>
     </body>
  </template>
 
 <style>
+    .containerxl_padding{
+    padding: 15px;
+}
     .input-inferior input{
-    border:blue;
     padding: 10px;
     margin: auto;
     width: 100%;
     border-radius: 15px;
     font-size: 16px;
+    background: #1d67cd;
 }
-.cont_fondo {
-    background: #b8b7b6;
-    padding-top: 5px;
+.color_top_edit {
+    background: #e8e6e6;
+    padding: 12px;
     padding-bottom: 5px;
     border-radius: 15px;
 }
+.color_back2_edit{
+    background: #78ecd5;
+}
+.color_back1_edit{
+    background: #abf8e5;
+}
+.focused{
+    padding: 17px;
+}
+.color_speaker_edit1{
+    background: #48a5d1;
+}
+.linea {
+    border-top: 2px solid rgb(191, 245, 229);
+    height: 2px;
+    padding: 0;
+    margin: 40px auto 0 auto;
+    margin-bottom: 40px;
+    overflow: auto;
+}
+
 </style>
