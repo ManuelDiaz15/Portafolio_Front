4d63ce791a53ce8aba7bfc03c7af84d11db24ec0
Jeremmy La Paz <je.lapaz@duocuc.cl>
2022-09-28 17:28:28 -0300

    Definición de estructura de la página lista transcripciones v0.3

--- src/views/Inicio/ListView.vue
+++ src/views/Inicio/ListView.vue
@@ -1,110 +1,157 @@
 <template>
-    <body class="color-body1">
-    <div class="d-flex">
-        <div class="container-fluid">
-            <div class="Title-list">
-        <h2>{{ message }}</h2>
+    <div>
+      <h2>Hola como estan {{ message }}</h2>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        item-key="name"
+        class="elevation-1"
+        :search=search
+        :custom-filter="filterOnlyCapsText"
+      >
+        <template v-slot:top>
+          <v-text-field
+            v-model=search
+            label="Buscar por id"
+            class="mx-4"
+          ></v-text-field>
+        </template>
+        <template body append>
+          <tr>
+            <td></td>
+            <td>
+              <v-text-field
+                v-model="calories"
+                type="number"
+                label="Less than"
+              ></v-text-field>
+            </td>
+            <td colspan="4"></td>
+          </tr>
+        </template>
+      </v-data-table>
     </div>
-    <div class="d-flex container-fluid">
-        <div class=" Back-Table">
-            <table class="table table-bordered table-responsive table-striped">
-				<thead class="table-head">
-					<tr>
-					<th scope="col">ID</th>
-					<th scope="col">Nivel de precición</th>
-					<th scope="col">Nombre del call center</th>
-					<th scope="col">Ejecutivo</th>
-					<th scope="col">Estado</th>
-                    <th scope="col">Fecha</th>
-					</tr>
-				</thead>
-				<tbody class="table-body">
-					<tr>
-					<th scope="row">1</th>
-					<td>70%</td>
-					<td>pepe</td>
-					<td>tomas</td>
-					<td>pendiente</td>
-                    <td>10/9/22</td>
-					</tr>
-				</tbody>
-            </table>
-        </div>
-        
-         </div>
-        
-        </div>
-        <div class="d-flex" >
-        <div class="ml-auto search-center1">
-            <div class="filter">
-                <p class="title-filter">Ingresa la ID de la transcripcion</p>
-                    <input  class="F-input" placeholder="ID-transcripción">
-                    <button class="btn-filter btn-secondary btn-sm">Buscar</button>
-            </div>
-        </div>
-    </div>
-    </div>
-    </body>
-</template>
-<script lang="ts">
+  </template>
+  <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
 
+    
+    
     @Component({
 		name: 'ListView',
     })
+    export class ListView extends Vue{
+        public message = this.$t("Main.message");
+    }  
 
-    export default class ListView extends Vue {
-		public message = this.$t("Main.message");
-	}
-</script>
-<style>
-/* fondo */
-.color-body1{
-    background-color: rgb(232 230 230);
-}
-/* titulo */
-.Title-list{
-    text-align: center;
-}
-/* filtro */
-.search-center1{
-    margin-top: 24%;
-}
-.filter{
-    background-color: #00A274;
-	border: 5px solid #C0C0C0;
-    width: 300px;
-    float: right;
-}
-.title-filter{
-    text-align: center;
-}
-.F-input{
-    background-color: #ffffff;
-    width: 60%;
-    height: 20px;
-}
-.btn-filter{
-    border: none;
-    text-align: right;
-    display: inline-block;
-    font-size: 16px;
-}
-/* Tamaño tabla */
-.Back-Table{
-    padding: 25px;
-    display: center;
-}
+    export default  {
 
-/* tabla cabeza */
-.table-head{
-    background-color: rgba(0,112,80,1);
-}
-/* tabla cuerpo */
-.table-body{
-    background-color: #ffffff;
-}
-
-
-
-</style>
+      data () {
+        return {
+          search: '',
+          calories: '',
+          desserts: [
+            {
+              id: 1,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 2,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 3,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 4,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 5,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 6,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 7,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 8,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 9,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+            {
+              id: 10,
+              callcenter: 'Falabella',
+              ejecutivo: 'Jose Perez',
+              estado: 'pendiente',
+              fecha: '10/12/22',
+            },
+          ],
+        }
+      },
+      computed: {
+        headers () {
+          return [
+            {
+              text: 'ID transcripción',
+              align: 'start',
+              sortable: false,
+              value: 'id',
+            },
+            { text: 'Callcenter', value: 'callcenter' },
+            { text: 'Ejecutivo', value: 'ejecutivo' },
+            { text: 'Estado', value: 'estado' },
+            { text: 'Fecha', value: 'fecha' },
+          ]
+        },
+      },
+      methods: {
+        filterOnlyCapsText (value, search, item) {
+          return value != null &&
+            search != null &&
+            typeof value == 'number' &&
+            value.toString().toLocaleUpperCase().indexOf(search) !== -1
+        },
+      },
+    }
+  </script>
+  <style>
+    .body{
+        background-color: #c0e9d5;
+    }
+  </style>
